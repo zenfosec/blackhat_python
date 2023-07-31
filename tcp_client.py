d66e880a740af42276225e96f68e2e258f94e868
Dave Howard <howard.dave@gmail.com>
2023-07-30 20:59:46 -0700

    first commit
    	new file:   tcp_client.py

---
+++ tcp_client.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+# create a socket object
+1 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+2 client.connect((target_host,target_port))
+
+# send some data
+3 client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# receive some data
+4 response = client.recv(4096)
+
+print(response.decode())
+client.close()
