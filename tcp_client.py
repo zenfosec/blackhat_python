380d01c4578d04e463189b10a47e81f4209d0b32
Dave Howard <howard.dave@gmail.com>
2023-07-31 15:12:01 -0700

    Fixing some typos

--- tcp_client.py
+++ tcp_client.py
@@ -1,21 +1,21 @@
 #!/bin/env python
 
-import socketgit commit -m "first commit"
+import socket
 
 target_host = "www.google.com"
 target_port = 80
 
 # create a socket object
-1 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the client
-2 client.connect((target_host,target_port))
+client.connect((target_host,target_port))
 
 # send some data
-3 client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
 # receive some data
-4 response = client.recv(4096)
+response = client.recv(4096)
 
 print(response.decode())
 client.close()
