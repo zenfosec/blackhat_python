2bc1ac4b0d2483a40845411ed8ea3618f6597b30
Dave Howard <howard.dave@gmail.com>
2023-07-31 15:12:17 -0700

    Fixing some typos

--- udp_client.py
+++ udp_client.py
@@ -6,13 +6,13 @@ target_host = "127.0.0.1"
 target_port = 9997
 
 # create a socket object
-1 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # send some data
-2 client.sendto(b"AAABBBCCC",(target_host,target_port))
+client.sendto(b"AAABBBCCC",(target_host,target_port))
 
 # receive some data
-3 data, addr = client.recvfrom(4096)
+data, addr = client.recvfrom(4096)
 
 print(data.decode())
 client.close()
